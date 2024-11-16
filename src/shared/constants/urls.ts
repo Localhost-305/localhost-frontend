2becfdf2e2a700901a1296804af7322cd82d7421
Lukas <lkfernando12345@gmail.com>
2024-11-16 11:11:18 -0300

    feature/LOC-72/atualizando os ip do backend, frontend e etl

--- src/shared/constants/urls.ts
+++ src/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_API_URL = 'http://34.228.159.68/api';
+const BASE_API_URL = 'http://54.227.92.222/api';
 
 export const URL_USER = `${BASE_API_URL}/users`;
 export const URL_AUTH = `${BASE_API_URL}/login`;
@@ -7,4 +7,4 @@ export const URL_CANDIDATES = `${BASE_API_URL}/candidates`;
 export const URL_APPLICATIONS = `${BASE_API_URL}/factApplication`;
 export const URL_HIRING = `${BASE_API_URL}/hiring`;
 // export const URL_UPLOAD = `${BASE_API_URL}/excel`;
-export const URL_UPLOAD = 'http://100.26.61.191:5000';
+export const URL_UPLOAD = 'http://54.167.100.252:5000';
