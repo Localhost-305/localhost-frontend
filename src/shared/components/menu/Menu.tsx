521eb3e1dbf9a63f08edc2b8550d3d0a3ea2ef1c
MCT <zoltancao@gmail.com>
2024-11-21 21:27:39 -0300

    feature/LOC-69 Atualização para o build

--- src/shared/components/menu/Menu.tsx
+++ src/shared/components/menu/Menu.tsx
@@ -3,12 +3,8 @@ import type { MenuProps } from 'antd';
 import { StyledMenu } from './menu.style';
 
 const Menu = () => {
-<<<<<<< HEAD
-  const items: MenuProps['items'] = [
-=======
-  const [current, setCurrent] = useState('Home');
+
   const items: MenuProps['items'] = [ 
->>>>>>> be692483424da41e4137d8956c2f4943ea55cda3
     {
       label: 'Home',
       key: 'Home',
